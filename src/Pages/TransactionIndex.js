e465a7cd8c10b12e43ffb27438a7f3dd613099f0
aardvarkpepper <aardvarkpepper@gmail.com>
2023-04-19 10:44:07 -0400

    Add src/Pages/TransactionIndex.js

---
+++ src/Pages/TransactionIndex.js
@@ -0,0 +1,10 @@
+import Transactions from "../Components/Transactions";
+
+export default function TransactionIndex() {
+    return (
+        <div className="TransactionIndex">
+            <h2>Index</h2>
+            <Bookmarks />
+        </div>
+    )
+}
