49e6d36a1d9287c31efc37b64219f6e747f538eb
aardvarkpepper <aardvarkpepper@gmail.com>
2023-04-19 10:44:53 -0400

    Fix typo in TransactionIndex.js

--- src/Pages/TransactionIndex.js
+++ src/Pages/TransactionIndex.js
@@ -4,7 +4,7 @@ export default function TransactionIndex() {
     return (
         <div className="TransactionIndex">
             <h2>Index</h2>
-            <Bookmarks />
+            <Transactions />
         </div>
     )
 }
