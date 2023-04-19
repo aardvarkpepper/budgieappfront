5075e8af7afe21afd01effaeac539886f7a0c79d
aardvarkpepper <aardvarkpepper@gmail.com>
2023-04-19 07:55:11 -0400

    Add Transaction.js

---
+++ src/Components/Transaction.js
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+
+export default function Bookmark({ transaction, index }) {
+    return (
+        <tr>
+            <td>
+                <Link to={`/transactions/${index}`}>{transaction.id}</Link>
+            </td>
+            <td>
+                <span>{transaction.item_name}</span>
+            </td>
+            <td>
+                <span>{transaction.amount}</span>
+            </td>
+            <td>
+                <span>{transaction.date}</span>
+            </td>
+            <td>
+                <span>{transaction.from}</span>
+            </td>
+            <td>
+                <span>{transaction.category}</span>
+            </td>
+        </tr>
+    )
+}
