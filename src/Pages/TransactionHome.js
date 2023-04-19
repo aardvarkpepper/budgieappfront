7711796e8c64cee92f244a3a8ed5c40709c61090
aardvarkpepper <aardvarkpepper@gmail.com>
2023-04-19 10:42:59 -0400

    Add src/Pages/TransactionHome.js

---
+++ src/Pages/TransactionHome.js
@@ -0,0 +1,8 @@
+export default function TransactionHome () {
+    return (
+        <div className = "Home">
+            <h2>Welcome</h2>
+            <div>To a Budget App</div>
+        </div>
+    )
+}
