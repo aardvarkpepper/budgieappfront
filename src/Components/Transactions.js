4cde0f37b12b0a5c022013b2a871ff539d9249e1
aardvarkpepper <aardvarkpepper@gmail.com>
2023-04-19 10:37:51 -0400

    Add Transactions.js

---
+++ src/Components/Transactions.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "React";
+import Transaction from "./Transaction";
+import axios from "axios";
+const API = process.env.REACT_APP_API_URL;
+
+export default function Transactions() {
+    const [transactions, setTransactions] = useState([]);
+    
+    useEffect(() => {
+        axios
+        .get(`${API}/transactions`)
+        .then((response) => setTransactions(response.data))
+        .catch((e) => console.error("catch", e))
+    }, []);
+
+    return (
+        <div className="Transactions">
+            <section>
+                <table>
+                    <tbody>
+                        {transactions.map((transaction, index) => {
+                            return <Transaction key={index} transaction={transaction} index={index} setTransactions={setTransactions} transactions = {transactions} />;
+                        })}
+                    </tbody>
+                </table>
+            </section>
+        </div>
+    )
+}
