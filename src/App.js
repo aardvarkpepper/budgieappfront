85271291ad07d42a1e8526f5b7e4266fef530446
aardvarkpepper <aardvarkpepper@gmail.com>
2023-04-19 07:26:00 -0400

    Add code to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Error404 from "./Pages/Error404";
+import Edit from "./Pages/TransactionEdit";
+import Home from "./Pages/TransactionHome";
+import Index from "./Pages/TransactionIndex";
+import New from "./Pages/TransactionNew";
+import Show from "./Pages/TransactionShow";
+
+import Navbar from "./Components/Navbar";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path ="/" element={<TransactionHome />} />
+          <Route path ="/transactions" element={<TransactionIndex />} />
+          <Route path ="/transactions/new" element={<TransactionNew />} />
+          <Route path ="/transactions/:index" element={<TransactionShow />} />
+          <Route path ="/transactions/:index/edit" element={<TransactionEdit />} />
+          <Route path ="*" element={<Error404 />} />
+        </Routes>
+      </main>
+      </BrowserRouter>
     </div>
   );
 }
