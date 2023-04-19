9e9e1733f4d83feea0066ee55305053ab277fd24
aardvarkpepper <aardvarkpepper@gmail.com>
2023-04-19 10:40:59 -0400

    Add src/Pages/TransactionEdit.js

---
+++ src/Pages/TransactionEdit.js
@@ -0,0 +1,10 @@
+import TransactionEditForm from "../Components/TransactionEditForm";
+
+export default function TransactionEditForm() {
+    return (
+        <div className="New Edit">
+        <h2>Edit</h2>
+        <TransactionEditForm />
+        </div>
+    )
+}
